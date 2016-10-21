ba3453b67ee440ddf6359ed1953ed20a8ccd92e0
SaeschM <sascha.mangelsen@web.de>
2016-10-21 11:32:57 +0200

    Binomische Formel

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,15 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		double a = 8;
+		double b = 3;
+		
+		double loesung = ((a*a)+(2*a*b)+(b*b)) ;
+		
+		System.out.println("(a+b)^2 =" + loesung);
+
+	}
+
+}
